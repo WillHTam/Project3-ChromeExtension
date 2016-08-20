691a80ee2c78051c1f7aad22793819ef01dbfbf7
William T <william.tam18@gmail.com>
2016-08-20 16:07:43 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Stashr
+## Chrome Extension (Ver 0.0.0.0.1)
+
+- Do 'bower install' inside the directory
+- Then in Chrome preferences, under Extensions, click 'Load unpacked extension' then navigate to your local directory where you cloned this repo and select it.
+- Doesn't work yet.  
---
+++ popup.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  page.init();
+  $('body').on('click', 'a', function(){
+     chrome.tabs.create({url: $(this).attr('href')});
+     return false;
+   });
+});
+
+
+var page = {
+  init: function () {
+    // chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    //   var tabUrl = tabs[0].url;
+    // });
+    page.initStyling();
+  },
+
+  initStyling: function () {
+    page.stash();
+  },
+
+  stash: function (event) {
+  $.ajax({
+    method: 'POST',
+    url: 'https://project3pockety.herokuapp.com/resources',
+    headers: {email: 'angel@angel.com', auth_token: 'af168421-d31d-410b-a5ba-bbc6bc4a2fb8'},
+    content: {url: 'http://edition.cnn.com/2016/08/19/sport/usain-bolt-rio-2016-olympics-legacy/index.html', tags: 'AddedWithStashr'},
+    success: function () {
+        $('.results').append('<p>Stashed!</p>');
+    }
+  });
+}
+};
